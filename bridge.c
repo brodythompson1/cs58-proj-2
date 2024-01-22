f5df34df450e39bb6040a9270679419e26c0c780
Brody Thompson <brody9thompson@gmail.com>
2024-01-22 13:44:11 -0500

    pushing starter code so I can switch to thayer

--- bridge.c
+++ bridge.c
@@ -4,3 +4,28 @@
  * Ledyard Bridge Construction Project
  * Implements Threads and Locks
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX_CARS 3
+
+#define TO_HANOVER 1
+#define TO_NORWICH 0
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+// taken basic structure from assignment details
+void OneVehicle(direction)
+{
+  ArriveBridge(direction);
+  // now the car is on the bridge!
+
+  OnBridge(direction);
+
+  ExitBridge(direction);
+  // now the car is off
+}
